c2bcf1996c30d655b023e3f58c91dfd675088154
GeraldBaldogo <baldogogerald@gmail.com>
2026-05-18 14:57:13 +0800

    nothing

--- profolio-frontend/src/services/api.js
+++ profolio-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://profolio-plzj.onrender.com/api',
+  baseURL: 'https://profolio-plzj.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
