ad612c2ac471874893265010ccbce63b51cb1530
GeraldBaldogo <baldogogerald@gmail.com>
2026-05-18 20:06:46 +0800

    debugging

--- profolio-frontend/src/services/api.js
+++ profolio-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: 'https://profolio-plzj.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
